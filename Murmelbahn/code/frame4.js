792a70793bc389f1d6ee829a84c3100fb5a491d8
Noah Hielscher <noah.hielscher@hfg.design>
2023-01-16 11:50:41 +0100

    Neustrukturierung und Outsourcen

---
+++ Murmelbahn/code/frame4.js
@@ -0,0 +1,61 @@
+function scene4() {
+	sceneBack.style["background"] = 'url("./frame4/background.png") no-repeat';
+
+	let kamin2, kamin3;
+
+	//ei
+	ei = new PolygonFromSVG(
+		world,
+		{
+			x: 420,
+			y: 0,
+			image: black,
+			fromFile: "./eggState/pathei.svg",
+			scale: 1,
+		},
+		{ label: "Murmel", isStatic: false, friction: 0.1, density: 0.001 }
+	);
+
+	//Path
+	kamin2 = new PolygonFromSVG(
+		world,
+		{
+			x: 730,
+			y: 270,
+			fromFile: "./frame4/kamin2a.svg",
+			scale: 1,
+		},
+		{ isStatic: true, friction: 0.0 }
+	);
+	blocks.push(kamin2);
+
+	//Path
+	kamin3 = new PolygonFromSVG(
+		world,
+		{
+			x: 520,
+			y: 225,
+			fromFile: "./frame4/kamin2b.svg",
+			scale: 1,
+		},
+		{ isStatic: true, friction: 0.0 }
+	);
+	blocks.push(kamin3);
+
+	//Automatischer Szenen wechsler
+	blocks.push(
+		new BlockCore(
+			world,
+			{
+				x: 620,
+				y: 720,
+				w: 600,
+				h: 40,
+				trigger: () => {
+					switchScene((scene + 1) % scenes.length);
+				},
+			},
+			{ isSensor: true, isStatic: true }
+		)
+	);
+}

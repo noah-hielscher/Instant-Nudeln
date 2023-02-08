ceb96a0a4c62c5e47a39b9a077dd551a2c233d15
Noah Hielscher <noah.hielscher@hfg.design>
2023-02-08 10:08:15 +0100

    gelbe Bälle und Mouse Drag Deaktiviert

--- Murmelbahn/code/sketch.js
+++ Murmelbahn/code/sketch.js
@@ -84,37 +84,37 @@ function setup() {
 	world = engine.world;
 
 	// add a mouse so that we can manipulate Matter objects
-	mouse = new Mouse(engine, canvas, { stroke: "blue", strokeWeight: 3 });
+	//mouse = new Mouse(engine, canvas, { stroke: "blue", strokeWeight: 3 });
 	//
 	// process mouseup events in order to drag objects or add more balls
-	mouse.on("startdrag", (evt) => {
-		isDrag = true;
-	});
-	mouse.on("mouseup", (evt) => {
-		if (!isDrag) {
-			let ball = new Ball(
-				world,
-				{
-					x: evt.mouse.position.x,
-					y: evt.mouse.position.y,
-					r: 15,
-					color: "yellow",
-				},
-				{
-					isStatic: false,
-					restitution: 0.9,
-					label: "Murmel",
-					density: 0.5,
-				}
-			);
-			Matter.Body.applyForce(ball.body, ball.body.position, {
-				x: 0.2,
-				y: -2,
-			});
-			blocks.push(ball);
-		}
-		isDrag = false;
-	});
+	//mouse.on("startdrag", (evt) => {
+	//	isDrag = true;
+	//});
+	//mouse.on("mouseup", (evt) => {
+	//	if (!isDrag) {
+	//		let ball = new Ball(
+	//			world,
+	//			{
+	//				x: evt.mouse.position.x,
+	//				y: evt.mouse.position.y,
+	//				r: 15,
+	//				color: "yellow",
+	//			},
+	//			{
+	//				isStatic: false,
+	//				restitution: 0.9,
+	//				label: "Murmel",
+	//				density: 0.5,
+	//			}
+	//		);
+	//		Matter.Body.applyForce(ball.body, ball.body.position, {
+	//			x: 0.2,
+	//			y: -2,
+	//		});
+	//		blocks.push(ball);
+	//	}
+	//	isDrag = false;
+	//});
 
 	// process collisions - checkt mit was die Murmel zusammenstößt
 	Events.on(engine, "collisionStart", function (event) {
